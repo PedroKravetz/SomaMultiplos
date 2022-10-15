829de4a25924f6008d2b6b6ec03af90f53d3e053
PedroKravetz <p.v.kravetz@gmail.com>
2022-10-15 13:56:44 -0300

    Java Doc e controle de número adicionado

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,16 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int numero, limite;
+        int numero = 0, limite;
         Scanner input = new Scanner(System.in);
-        System.out.println("Digite o número: ");
-        numero = input.nextInt();
+        while (numero <= 0) {
+            System.out.println("Digite o número: ");
+            numero = input.nextInt();
+        }
         System.out.println("Digite o limite: ");
         limite = input.nextInt();
         System.out.println(somaInterativa(numero, limite));
         System.out.println(somaRecursiva(numero, numero, limite));
     }
 
+    /**
+     * Executa a soma dos múltiplos do número até o limite especificado recursivamente
+     *
+     * @param numero   int - O número que será incrementado para descobrir os múltiplos
+     * @param controle int - O número constante que incrementará o numero para fazer todos os múltiplos
+     * @param limite   int - O limite pelo qual o número poderá ser incrementado
+     * @return int - A soma dos múltiplos ou 0 caso número seja maior que o limite
+     */
     private static int somaRecursiva(int numero, int controle, int limite) {
         if (numero <= limite) {
             return numero + somaRecursiva(numero + controle, controle, limite);
@@ -21,6 +31,13 @@ public class Main {
         return 0;
     }
 
+    /**
+     * Executa a soma dos múltiplos do número até o limite especificado iterativamente
+     *
+     * @param numero int - O número que será somados os múltiplos dele
+     * @param limite int - O limite para qual o múltiplo do numero não pode passar
+     * @return int - A soma dos múltiplos
+     */
     private static int somaInterativa(int numero, int limite) {
         int controle = numero;
         int soma = 0;
